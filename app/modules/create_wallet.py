5ad77f489d8511740a824bd79724163e8a3c6235
IgorTatarinov <ig.tatarinov@gmail.com>
2022-07-06 17:25:01 +0300

    Function reworked

--- app/modules/create_wallet.py
+++ app/modules/create_wallet.py
@@ -3,10 +3,10 @@ import requests
 import uuid
 
 
-def create_wallet_send_data(jwtToken, uid, first_name, last_name, email, phone):
+def create_wallet_send_data(jwtToken):
     URL = "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/guest"
     # generate session and jwt from uid
-    session_id = uuid.uuid1()
+    session_id = str(uuid.uuid1())
     encoded_jwt = jwtToken
     # create headers
     headers = {'X-Auth-Token': encoded_jwt,
