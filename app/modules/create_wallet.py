25965dbe518cf0069dab054338005605e9384cee
IgorTatarinov <ig.tatarinov@gmail.com>
2022-07-23 11:06:21 +0300

    url fix

--- app/modules/create_wallet.py
+++ app/modules/create_wallet.py
@@ -4,7 +4,7 @@ import uuid
 
 
 def create_wallet_send_data(jwtToken):
-    URL = "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/sign-up"
+    URL = "https://wallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/sign-up"
     # generate session and jwt from uid
     session_id = str(uuid.uuid1())
     encoded_jwt = jwtToken
@@ -20,7 +20,7 @@ def create_wallet_send_data(jwtToken):
 
 
 def activate_wallet(resp, encoded_jwt, session_id):
-    URL = 'https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account'
+    URL = 'hhttps://wallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account'
     authorisation_key = resp["access_token"]
 
     headers = {'Content-Type': 'text/application/json; charset=utf-8',
@@ -38,7 +38,7 @@ def activate_wallet(resp, encoded_jwt, session_id):
 
 
 def sign_in_wallet_send_data(jwtToken):
-    URL = "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/sign-in"
+    URL = "https://wallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/sign-in"
 
     session_id = str(uuid.uuid1())
     encoded_jwt = jwtToken
@@ -53,7 +53,7 @@ def sign_in_wallet_send_data(jwtToken):
 def get_wallet_balance(jwtToken, guid):
     sign_in_data = sign_in_wallet_send_data(jwtToken)
     header = {'Authorization': 'Bearer ' + sign_in_data["access_token"]}
-    URL = "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account/"
+    URL = "https://wallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account/"
     request = requests.get(URL + guid + "?include_assets=true", headers=header)
     return request.json()
 
@@ -62,7 +62,7 @@ def withdraw_from_wallet(jwtToken, amount, guid):
     sign_in_data = sign_in_wallet_send_data(jwtToken)
     merchant_guid = "af74cf04-311f-435e-a530-0c85fbd6d154"
     header = {'Authorization': 'Bearer ' + sign_in_data["access_token"]}
-    URL = "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account/"
+    URL = "https://wallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account/"
     data = {
         'amount': float(amount),
         'asset':'ATUSD:GBT4VVTDPCNA45MNWX5G6LUTLIEENSTUHDVXO2AQHAZ24KUZUPLPGJZH',
@@ -87,7 +87,7 @@ def authPayment(transaction, guid, operandID, Authorization, AuthToken, SessionI
         'X-Auth-Token': AuthToken,
         'X-SessionID': str(SessionID)
     }
-    URL = "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account/"
+    URL = "https://wallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/user/platform/stellar/account/"
 
     data = {
         "transaction_status": "pending",
