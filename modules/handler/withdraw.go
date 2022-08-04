8853def7445fb9d07a8aa24d26200bb5d670244b
anton <anton.tatarinov22@gmail.com>
2022-08-04 15:03:08 +0300

    Commited

--- modules/handler/withdraw.go
+++ modules/handler/withdraw.go
@@ -6,5 +6,8 @@ import (
 )
 
 func Withdraw(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	jwtToken := r.Header.Get("auth_key")
+	guid := r.Header.Get("guid")
+	amount := r.Header.Get("amount")
 
 }
