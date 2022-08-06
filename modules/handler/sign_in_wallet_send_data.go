5359a867845e2185de182eb5804a3e33b834dd28
anton <anton.tatarinov22@gmail.com>
2022-08-06 16:31:38 +0300

    Commited

---
+++ modules/handler/sign_in_wallet_send_data.go
@@ -0,0 +1,22 @@
+package handler
+
+func sign_in_wallet_send_data(jwtToken string) {
+	/*
+		URL := "https://atwallet.rock-west.net/api/v1/wallet/application/ab54ee14-15f1-4ce5-bcc3-6559451354da/sign-up"
+		// generate session and jwt from uid
+		sessionID, _ := uuid.NewUUID()
+		// create headers
+		client := http.Client{}
+		request, err := http.NewRequest("POST", URL, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		request.Header.Set("X-Auth-Token", jwtToken)
+		request.Header.Set("X-Session-ID", sessionID.String())
+		// make a request
+		token := ATToken{}
+		activateWallet(token.AccessToken, jwtToken, sessionID.String())
+
+	*/
+}
