8bbbaf019896c9d90c1211d0b9f0cd13621423ec
Vladimir Tatarinov <vladimir.tatarinov@rock-west.com>
2022-08-12 11:13:21 +0300

    removed middleware for testing

--- modules/middleware/middleware.go
+++ modules/middleware/middleware.go
@@ -2,28 +2,27 @@ package middleware
 
 import (
 	"birdhouse/modules/service"
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"time"
 )
 
 func AuthMiddleware(atWallet *service.ATWalletService, next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		authToken := r.URL.Query().Get("auth_key")
-		_, issuedAt, expiresAt, err := atWallet.TokenDecode(authToken)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
+		/*
+			authToken := r.URL.Query().Get("auth_key")
+				_, issuedAt, expiresAt, err := atWallet.TokenDecode(authToken)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
 
-		if expiresAt.Before(time.Now()) {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
+				if expiresAt.Before(time.Now()) {
+					w.WriteHeader(http.StatusUnauthorized)
+					return
+				}
 
-		fmt.Print(issuedAt, expiresAt)
-
+				fmt.Print(issuedAt, expiresAt)
+		*/
 		next(w, r.WithContext(r.Context()), ps)
 	}
 }
